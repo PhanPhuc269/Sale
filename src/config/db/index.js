d927e70e28f838bba0c14e488cebd8e7c18be122
Phan Hong Phuc <22120280@student.hcmus.edu.vn>
2024-08-20 20:56:55 +0700

    web

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -6,6 +6,7 @@ async function connect() {
         await mongoose.connect('mongodb+srv://2php2692004:conchimlahet@du-an-dau-tay.3zmkc.mongodb.net/?retryWrites=true&w=majority&appName=du-an-dau-tay/my-education-dev', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            setTimeout: 30000,
         });
         console.log('Connect successfully!!!');
     } catch (error) {
