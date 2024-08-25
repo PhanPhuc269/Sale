660fcdfa4d49d099f0181ecbd82a026e48c784d6
Phan Hong Phuc <22120280@student.hcmus.edu.vn>
2024-08-25 16:06:22 +0700

    web-final

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const path = require('path');  
 require('dotenv').config({ path: path.join(__dirname, '../../MONGODB.env') });
 
-//console.log(process.env.MONGODB);
+//console.log();'mongodb://localhost:27017/my-education-dev'
 //'mongodb+srv://2php2692004:conchimlahet@du-an-dau-tay.3zmkc.mongodb.net/?retryWrites=true&w=majority&appName=du-an-dau-tay/my-education-dev'
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/my-education-dev', {
+        await mongoose.connect(process.env.MONGODB, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             serverSelectionTimeoutMS: 10000
